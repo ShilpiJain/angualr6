4f23e1342c21bc33b16ab9dc29d6b9568372b940
shijain <jain.shilpi328@gmail.com>
2018-09-18 22:23:11 +0530

    routing and components structure [about, core, customer, customers, login.order]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { CoreComponent } from './core/core.component';
+import { CustomerComponent } from './customer/customer.component';
+import { CustomersComponent } from './customers/customers.component';
+import { OrdersComponent } from './orders/orders.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    CoreComponent,
+    CustomerComponent,
+    CustomersComponent,
+    OrdersComponent
   ],
   imports: [
     BrowserModule
